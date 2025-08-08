9bc0f1c366e54b2038d81d9c411f64524757b601
ooncel <ogul.oncel@proton.me>
2025-08-08 13:06:42 +0200

    Update ged2print.py
    Adding birth/death date/place informations and styling occupation string such that it will become multiline when string is longer than full name of the person.

--- ged2print.py
+++ ged2print.py
@@ -1,13 +1,14 @@
 from ged4py import GedcomReader
 import graphviz
 import os
+import textwrap
 from collections import defaultdict
 
 debug = 0
 
 # Initiate the graph
 dot = graphviz.Digraph(comment="Inferred Spouse Pairs", format='pdf')
-dot.attr(fontsize='30', arrowhead='none', nodesep='1', ranksep='3',rankdir='TB',size = "33.11,23.39")
+dot.attr(fontsize='10', arrowhead='none', nodesep='1', ranksep='2',rankdir='TB',size = "33.11,23.39")
 
 # Path of the GEDCOM file
 path = "/Users/oncel/Desktop/tayyarnew.ged"
@@ -47,9 +48,6 @@ with GedcomReader(path) as parser:
     # Add individual nodes
 
     for i_id, indi in individuals.items():
-        #print('i_id:',i_id)
-        #if i_id in {f"@I{str(i).zfill(4)}@" for i in range(68, 112)}:
-        #if i_id != '@I0066@':
             name = indi.sub_tag_value("NAME")
             if name and '/' in name:
                 parts = name.split('/')
@@ -61,6 +59,32 @@ with GedcomReader(path) as parser:
             gender = indi.sub_tag_value("SEX")
             occup = indi.sub_tag_value("OCCU")
             label = " ".join(part for part in name if part).replace("/", "") if isinstance(name, tuple) else name
+            #if occup:
+            #    occup='<BR/>'.join(occup[i:i+len(label)] for i in range(0, len(occup), len(label)))
+            if occup:
+                mult = 1
+                new_str = ""
+                for i, char in enumerate(occup):
+                    if i > len(label) * mult and char == ",":
+                        new_str += "<BR/>"
+                        mult += 1
+                    else:
+                        new_str += char
+                occup = new_str
+                print(occup)
+            #print('newoccup:',occup)
+            birth_date = indi.sub_tag_value("BIRT/DATE")
+            birth_place = indi.sub_tag_value("BIRT/PLAC")
+            death_date = indi.sub_tag_value("DEAT/DATE")
+            death_place = indi.sub_tag_value("DEAT/PLAC")
+            if not birth_date:
+                birth_date=' '
+            if not birth_place:
+                birth_place=' '
+            if not death_date:
+                death_date=' '
+            if not death_place:
+                death_place=' '
             shape = "ellipse" if gender == "F" else "box"
             image_path = indi_image_paths.get(i_id)
             if debug>0:
@@ -69,24 +93,24 @@ with GedcomReader(path) as parser:
                 dot.node(i_id, label=f'''<
 <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
   <TR> <TD FIXEDSIZE="TRUE" WIDTH="600" HEIGHT="600"><IMG SRC="{image_path}" SCALE="TRUE"/></TD></TR>
-  <TR><TD><FONT POINT-SIZE="100">{label}<BR/>{occup}</FONT></TD></TR>
+  <TR><TD><FONT POINT-SIZE="100">{label}<BR/>{birth_date},{birth_place}-{death_date},{death_place}<BR/><i>{occup}</i></FONT></TD></TR>
 </TABLE>
 >''', shape='none')
             elif image_path and os.path.isfile(image_path) and not occup:
-                 dot.node(i_id, label=f'''<
+                dot.node(i_id, label=f'''<
 <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
   <TR> <TD FIXEDSIZE="TRUE" WIDTH="600" HEIGHT="600"><IMG SRC="{image_path}" SCALE="TRUE"/></TD></TR>
-  <TR><TD><FONT POINT-SIZE="100">{label}</FONT></TD></TR>
+  <TR><TD><FONT POINT-SIZE="100">{label}<BR/>{birth_date},{birth_place}-{death_date},{death_place}<BR/></FONT></TD></TR>
 </TABLE>
->''', shape='none')               
+>''', shape='none')             
             else:
                 #dot.node(i_id, label=label, shape=shape)
                 dot.node(i_id, label=f'''<
 <TABLE BORDER="0" CELLBORDER="1" CELLSPACING="0">
   <TR> <TD FIXEDSIZE="TRUE" WIDTH="600" HEIGHT="600"></TD></TR>
-  <TR><TD><FONT POINT-SIZE="100">{label}</FONT></TD></TR>
+  <TR><TD><FONT POINT-SIZE="100">{label}<BR/>{birth_date},{birth_place}-{death_date},{death_place}<BR/></FONT></TD></TR>
 </TABLE>
->''', shape='none')
+>''', shape='none') 
 
     # Infer couples: anyone who shares a FAMS is likely a couple
     for fam_id, people in fams_to_people.items():
@@ -101,12 +125,15 @@ with GedcomReader(path) as parser:
                     s.attr(rank='same')
                     s.node(p1.xref_id)
                     s.node(p2.xref_id)
-                    if fam_id in {f"@F{str(i).zfill(4)}@" for i in range(10, 10)} or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(34, 40)} or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(5, 10)}:
-                    #    print(fam_id)
-                    #if fam_id=='@F0000@' or fam_id=='@F0001@' or fam_id=='@F0002@' or fam_id=='@F0003@' or fam_id=='@F0004@'or fam_id=='@F0005@' or fam_id=='@F0006@' or fam_id=='@F0007@' or fam_id=='@F0008@' or fam_id=='@F0009@' or fam_id=='@F0015@' or fam_id=='@F0016@'or fam_id=='@F0033@' or fam_id=='@F0034@' or fam_id=='@F0039@' or fam_id=='F0035':
+                    #if fam_id=='@F0010@' or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(34, 40)} or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(5, 10)}:
+                    if fam_id in {f"@F{str(i).zfill(4)}@" for i in range(34, 36)} or fam_id =='@F0039@' or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(5, 8)}:
+                        minlen_parents_str='0'
+                    elif fam_id =='@F0009@':
+                        minlen_parents_str='0'
+                    elif fam_id =='@F0033@':
                         minlen_parents_str='0'
                     else:
-                        minlen_parents_str='2'
+                        minlen_parents_str='1'
                     dot.edge(p1.xref_id, marriage_node, dir="none", style="solid",minlen=minlen_parents_str)
                     dot.edge(marriage_node, p2.xref_id, dir="none", style="solid",minlen=minlen_parents_str)
                     if debug>0:
@@ -115,13 +142,23 @@ with GedcomReader(path) as parser:
                     if fam_id==fam_idchild:
                         if debug>0:
                             print('n children:',len(children))
-                        minlen_str='2'
+                        if fam_id in {f"@F{str(i).zfill(4)}@" for i in range(34, 36)} or fam_id =='@F0039@' or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(5, 7)}:
+                        #if fam_id in {f"@F{str(i).zfill(4)}@" for i in range(34, 40)} or fam_id in {f"@F{str(i).zfill(4)}@" for i in range(5, 10)}:
+                            minlen_child_str='2'
+                        elif fam_id =='@F0009@': #m. izzet ebeveyn
+                            minlen_child_str='2'
+                        elif fam_id =='@F0008@': 
+                            minlen_child_str='1'
+                        elif fam_id =='@F0033@': # m. izzet- cerkes seref
+                            minlen_child_str='3'
+                        else:
+                            minlen_child_str='1'
                         for c in range(0,len(children)):
                            c1 = children[c]
-                           dot.edge(marriage_node, c1.xref_id, arrowhead='none',concentrate='true',minlen=minlen_str)
+                           dot.edge(marriage_node, c1.xref_id, arrowhead='none',concentrate='true',minlen=minlen_child_str)
             elif len(people) > 2:
                 print(f"Family {fam_id} has more than 2 spouses?! -> {[p.xref_id for p in people]}")
 print("done.")
 #dot.unflatten(stagger=3)
-dot.render("eminesitki", view=True)
-print("Rendered inferred_spouses.pdf")
+dot.render("hede", view=True)
+print("hede.pdf")
